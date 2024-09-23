70a8b0dcfeef71886034f9e49cda4fc7aed8348a
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-09-23 14:09:46 +0200

    feat: saving movies to csv, concurrent API requests for titles

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-use std::env;
+use std::{env, fs::File, path::Path};
 
 use anyhow::Context;
-use api::entity_to_movie;
+use api::{entity_to_movie, MovieRating};
+use csv::WriterBuilder;
+use futures::future::try_join_all;
 
 mod api;
 mod error;
 mod util;
 
 static BASE_URL: &str = "https://www.filmweb.pl/api/v1";
+
+fn movies_to_csv(
+    file_path: &Path,
+    movies: &[MovieRating],
+) -> anyhow::Result<()> {
+    let out = File::create(file_path)?;
+    let mut writer = WriterBuilder::new().delimiter(b';').from_writer(out);
+
+    for movie in movies {
+        writer.serialize(movie)?;
+    }
+
+    writer.flush()?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -17,11 +34,9 @@ async fn main() -> anyhow::Result<()> {
         env::var("COOKIE_HEADER").expect("COOKIE_HEADER should be set");
 
     let ratings = api::fetch_ratings(&cookie_header).await?;
+    let movies = try_join_all(ratings.iter().map(entity_to_movie)).await?;
 
-    for rating in ratings {
-        let movie_info = entity_to_movie(&rating).await?;
-        println!("{:#?}", movie_info);
-    }
+    movies_to_csv(Path::new("exports.csv"), &movies)?;
 
     Ok(())
 }
