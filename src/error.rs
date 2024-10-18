8e3029d37e303f2ac501d5b6fee450cc5411091e
Paweł Rybak <87527837+wedkarz02@users.noreply.github.com>
2024-10-18 09:36:07 +0200

    feat: AppError with ApiError variant, close (#4)

--- src/error.rs
+++ src/error.rs
@@ -6,9 +6,24 @@ pub enum ApiError {
     Unauthorized,
     BadRequest,
     InternalServerError,
+    Unrecognized,
+}
+
+#[derive(Debug)]
+pub enum AppError {
+    ApiError(ApiError),
     ReqwestError(reqwest::Error),
     SerdeJsonError(serde_json::Error),
-    Unrecognized,
+}
+
+impl Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ApiError(err) => write!(f, "{}", err),
+            Self::ReqwestError(err) => write!(f, "reqwest error: {}", err),
+            Self::SerdeJsonError(err) => write!(f, "serde_json error: {}", err),
+        }
+    }
 }
 
 impl Display for ApiError {
@@ -20,31 +35,37 @@ impl Display for ApiError {
             }
             Self::BadRequest => write!(f, "Invalid or expired tokens"),
             Self::InternalServerError => write!(f, "Internal server error"),
-            Self::ReqwestError(err) => write!(f, "reqwest error: {}", err),
-            Self::SerdeJsonError(err) => write!(f, "serde_json error: {}", err),
             Self::Unrecognized => write!(f, "Unrecognized error"),
         }
     }
 }
 
-impl Error for ApiError {
+impl Error for ApiError {}
+
+impl Error for AppError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            ApiError::ReqwestError(ref err) => Some(err),
-            ApiError::SerdeJsonError(ref err) => Some(err),
+            AppError::ReqwestError(ref err) => Some(err),
+            AppError::SerdeJsonError(ref err) => Some(err),
             _ => None,
         }
     }
 }
 
-impl From<reqwest::Error> for ApiError {
-    fn from(value: reqwest::Error) -> Self {
-        ApiError::ReqwestError(value)
+impl From<ApiError> for AppError {
+    fn from(value: ApiError) -> Self {
+        AppError::ApiError(value)
     }
 }
 
-impl From<serde_json::Error> for ApiError {
+impl From<reqwest::Error> for AppError {
+    fn from(value: reqwest::Error) -> Self {
+        AppError::ReqwestError(value)
+    }
+}
+
+impl From<serde_json::Error> for AppError {
     fn from(value: serde_json::Error) -> Self {
-        ApiError::SerdeJsonError(value)
+        AppError::SerdeJsonError(value)
     }
 }
